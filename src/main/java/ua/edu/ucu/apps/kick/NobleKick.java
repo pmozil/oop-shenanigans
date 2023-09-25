291a3dac8d8442cd7a8ddbbbd08e8dbbdaa2f38d
Petro Mozil <mozil.petryk@gmail.com>
2023-09-25 14:37:40 +0300

    Fix the compile errors

--- src/main/java/ua/edu/ucu/apps/kick/NobleKick.java
+++ src/main/java/ua/edu/ucu/apps/kick/NobleKick.java
@@ -5,8 +5,8 @@ import ua.edu.ucu.apps.lotr.Noble;
 
 public class NobleKick implements KickStrategy {
     @Override
-    public void kick(Noble kicker, Character opponent) {
-        int dec = kicker.random();
+    public void kick(Character kicker, Character opponent) {
+        int dec = ((Noble) kicker).random();
         
         opponent.setHp(opponent.getHp() - dec);
     }
